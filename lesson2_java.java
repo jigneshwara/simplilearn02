829115cf4bebfd6d2ec3d5ed15f4cebf8d85a16b
jigneshwara <jignesh.wara@edenred.com>
2021-01-16 12:45:44 +0530

    Push all the files to my GitHUB

---
+++ lesson2_java.java
@@ -0,0 +1,8 @@
+class lesson2{
+
+public static void main(String arg[])
+{
+System.out.println("This is my first print screen of Lesson 2");
+}
+
+}
---
+++ lesson2_javascript.js
@@ -0,0 +1,3 @@
+function msg(){  
+ window.alert("This is my first JS file from Lesson2");  
+}  
